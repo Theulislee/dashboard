1bc8e3064746a26b4a7f38e61b3be6ef5154ca6f
Matheus Magalhães <magalhaescerqueiraesilva@gmail.com>
2022-01-19 20:09:30 -0300

    Criado o component do dashboard.

--- src/app/dashboard/index.ts
+++ src/app/dashboard/index.ts
@@ -1,1 +1,2 @@
-export * from './dashboard.module'; //export do arquivo dashboard
+export * from './dashboard.module'; //export do arquivo dashboard.module
+export * from './dashboard.component'; //export do arquivo dashboard.component
